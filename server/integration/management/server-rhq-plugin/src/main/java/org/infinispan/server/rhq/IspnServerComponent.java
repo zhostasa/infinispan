544eb0ac0a4d5928be9f6773520a0390e91c72d6
Sebastian Laskawiec <slaskawi@redhat.com>
2016-03-14 12:43:31 +0100

    JDG-71 RHQ Server Plugin

---
+++ server/integration/management/server-rhq-plugin/src/main/java/org/infinispan/server/rhq/IspnServerComponent.java
@@ -0,0 +1,7 @@
+package org.infinispan.server.rhq;
+
+import org.rhq.core.pluginapi.measurement.MeasurementFacet;
+
+public class IspnServerComponent extends MetricsRemappingComponent<IspnServerComponent> implements MeasurementFacet {
+
+}
