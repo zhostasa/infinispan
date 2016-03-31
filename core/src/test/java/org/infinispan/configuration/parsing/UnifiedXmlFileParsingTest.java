38de345c0a7c6a14ba697495b495f296e5b4ef41
Vaclav Dedik <vdedik@redhat.com>
2016-03-31 15:20:36 +0200

    JDG-8 xsd schema version changed to 8.3

--- core/src/test/java/org/infinispan/configuration/parsing/UnifiedXmlFileParsingTest.java
+++ core/src/test/java/org/infinispan/configuration/parsing/UnifiedXmlFileParsingTest.java
@@ -46,7 +46,7 @@ public class UnifiedXmlFileParsingTest extends AbstractInfinispanTest {
 
    @DataProvider(name = "configurationFiles")
    public Object[][] configurationFiles() {
-      return new Object[][] { {"7.0.xml"}, {"7.1.xml"}, {"7.2.xml"}, {"8.0.xml"}, {"8.1.xml"}, {"8.2.xml"} };
+      return new Object[][] { {"7.0.xml"}, {"7.1.xml"}, {"7.2.xml"}, {"8.0.xml"}, {"8.1.xml"}, {"8.2.xml"}, {"8.3.xml"} };
    }
 
    @Test(dataProvider="configurationFiles")
