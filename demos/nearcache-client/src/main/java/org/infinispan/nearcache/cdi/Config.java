b87bbc2d97c5e5b45515956fbace15bbbd64ab2a
Sebastian Laskawiec <slaskawi@redhat.com>
2016-05-11 11:07:13 +0200

    JDG-208 ISPN-5900 CDI split up (common, remote and embedded)
    # Conflicts:
    #	cdi/embedded/src/test/java/org/infinispan/cdi/embedded/test/cachemanager/programmatic/ProgrammaticCacheContainerTest.java

--- demos/nearcache-client/src/main/java/org/infinispan/nearcache/cdi/Config.java
+++ demos/nearcache-client/src/main/java/org/infinispan/nearcache/cdi/Config.java
@@ -1,12 +1,12 @@
 package org.infinispan.nearcache.cdi;
 
-import javax.enterprise.inject.Produces;
-
-import org.infinispan.cdi.ConfigureCache;
+import org.infinispan.cdi.embedded.ConfigureCache;
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.eviction.EvictionStrategy;
 import org.infinispan.nearcache.jms.RemoteEventStoreConfigurationBuilder;
+
+import javax.enterprise.inject.Produces;
 
 /**
  * Configuration of the cache
