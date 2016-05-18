51e9e103490a33cfc5f17259eb7130251a9fc688
Tristan Tarrant <ttarrant@redhat.com>
2016-05-18 20:09:27 +0200

    JDG-396 ISPN-6652 Deprecate all evictionThreadPool and add expirationThreadPool

--- core/src/test/java/org/infinispan/configuration/XmlFileParsingTest.java
+++ core/src/test/java/org/infinispan/configuration/XmlFileParsingTest.java
@@ -356,7 +356,7 @@ public class XmlFileParsingTest extends AbstractInfinispanTest {
       assertEquals("TotalOrderValidatorThread", totalOrderThreadFactory.threadNamePattern());
 
       DefaultThreadFactory evictionThreadFactory =
-            cm.getCacheManagerConfiguration().evictionThreadPool().threadFactory();
+            cm.getCacheManagerConfiguration().expirationThreadPool().threadFactory();
       assertEquals("ExpirationThread", evictionThreadFactory.threadNamePattern());
 
       DefaultThreadFactory replicationQueueThreadFactory =
