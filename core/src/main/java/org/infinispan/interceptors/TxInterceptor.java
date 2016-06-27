3588794098828ff5a69e39ec5903d90f746edc66
Dan Berindei <dan@infinispan.org>
2016-06-27 14:32:23 +0300

    JDG-392 ISPN-6800 Remove MultipleRpcCommand

--- core/src/main/java/org/infinispan/interceptors/TxInterceptor.java
+++ core/src/main/java/org/infinispan/interceptors/TxInterceptor.java
@@ -467,8 +467,7 @@ public class TxInterceptor<K, V> extends CommandInterceptor implements JmxStatis
       final GlobalTransaction globalTransaction = command.getGlobalTransaction();
 
       // command.getOrigin() and ctx.getOrigin() are not reliable for LockControlCommands started by
-      // ClusteredGetCommands, or for PrepareCommands started by MultipleRpcCommands (when the replication queue
-      // is enabled).
+      // ClusteredGetCommands
       final Address origin = globalTransaction.getAddress();
 
       //It is possible to receive a prepare or lock control command from a node that crashed. If that's the case rollback
