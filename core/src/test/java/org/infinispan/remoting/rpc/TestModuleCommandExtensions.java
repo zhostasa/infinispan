ca8aa23384a59eb7ed8a04104ce83517b8de37a9
Adrian Nistor <anistor@redhat.com>
2016-08-18 17:25:50 +0300

    JDG-547 ISPN-5833 Remove deprecated class ExtendedModuleCommandFactory

--- core/src/test/java/org/infinispan/remoting/rpc/TestModuleCommandExtensions.java
+++ core/src/test/java/org/infinispan/remoting/rpc/TestModuleCommandExtensions.java
@@ -2,8 +2,8 @@ package org.infinispan.remoting.rpc;
 
 import org.infinispan.Cache;
 import org.infinispan.commands.ReplicableCommand;
-import org.infinispan.commands.module.ExtendedModuleCommandFactory;
 import org.infinispan.commands.module.ModuleCommandExtensions;
+import org.infinispan.commands.module.ModuleCommandFactory;
 import org.infinispan.commands.module.ModuleCommandInitializer;
 import org.infinispan.commands.remote.CacheRpcCommand;
 import org.infinispan.factories.annotations.Inject;
@@ -19,11 +19,11 @@ import java.util.Map;
 public class TestModuleCommandExtensions implements ModuleCommandExtensions {
 
    @Override
-   public ExtendedModuleCommandFactory getModuleCommandFactory() {
-      return new ExtendedModuleCommandFactory() {
+   public ModuleCommandFactory getModuleCommandFactory() {
+      return new ModuleCommandFactory() {
          @Override
          public Map<Byte, Class<? extends ReplicableCommand>> getModuleCommands() {
-            Map<Byte, Class<? extends ReplicableCommand>> map = new HashMap<Byte, Class<? extends ReplicableCommand>>(2);
+            Map<Byte, Class<? extends ReplicableCommand>> map = new HashMap<>(2);
             map.put(CustomReplicableCommand.COMMAND_ID, CustomReplicableCommand.class);
             map.put(CustomCacheRpcCommand.COMMAND_ID, CustomCacheRpcCommand.class);
             map.put(SleepingCacheRpcCommand.COMMAND_ID, SleepingCacheRpcCommand.class);
