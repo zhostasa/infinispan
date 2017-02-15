340b56d6966f260095ef7d86ef5bab2dd8cb1e4a
Radim Vansa <rvansa@redhat.com>
2017-02-15 14:08:00 +0100

    ISPN-5021 Rename CacheTopologyControlCommand.REBALANCE_CONFIRM to REBALANCE_PHASE_CONFIRM (and related stuff)

--- core/src/test/java/org/infinispan/distribution/rehash/NonTxStateTransferOverwritingValueTest.java
+++ core/src/test/java/org/infinispan/distribution/rehash/NonTxStateTransferOverwritingValueTest.java
@@ -29,8 +29,6 @@ import org.infinispan.test.fwk.CheckPoint;
 import org.infinispan.topology.ClusterTopologyManager;
 import org.infinispan.transaction.TransactionMode;
 import org.infinispan.tx.dld.ControlledRpcManager;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 import org.testng.annotations.Test;
 
 /**
@@ -194,17 +192,14 @@ public class NonTxStateTransferOverwritingValueTest extends MultipleCacheManager
          throws Exception {
       ClusterTopologyManager ctm = TestingUtil.extractGlobalComponent(manager, ClusterTopologyManager.class);
       ClusterTopologyManager spyManager = spy(ctm);
-      doAnswer(new Answer<Object>() {
-         @Override
-         public Object answer(InvocationOnMock invocation) throws Throwable {
-            Object[] arguments = invocation.getArguments();
-            Address source = (Address) arguments[1];
-            int topologyId = (Integer) arguments[2];
-            checkPoint.trigger("pre_rebalance_confirmation_" + topologyId + "_from_" + source);
-            checkPoint.awaitStrict("resume_rebalance_confirmation_" + topologyId + "_from_" + source, 10, SECONDS);
-            return invocation.callRealMethod();
-         }
-      }).when(spyManager).handleRebalanceCompleted(anyString(), any(Address.class), anyInt(), any(Throwable.class),
+      doAnswer(invocation -> {
+         Object[] arguments = invocation.getArguments();
+         Address source = (Address) arguments[1];
+         int topologyId = (Integer) arguments[2];
+         checkPoint.trigger("pre_rebalance_confirmation_" + topologyId + "_from_" + source);
+         checkPoint.awaitStrict("resume_rebalance_confirmation_" + topologyId + "_from_" + source, 10, SECONDS);
+         return invocation.callRealMethod();
+      }).when(spyManager).handleRebalancePhaseConfirm(anyString(), any(Address.class), anyInt(), any(Throwable.class),
             anyInt());
       TestingUtil.replaceComponent(manager, ClusterTopologyManager.class, spyManager, true);
    }
