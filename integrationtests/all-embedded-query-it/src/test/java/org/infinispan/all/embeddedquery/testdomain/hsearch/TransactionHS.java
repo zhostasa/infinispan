c26312d82b8455a386a11ebc2c2ff3b4af628d5b
Adrian Nistor <anistor@redhat.com>
2017-06-02 17:51:58 +0300

    Some more @SortableField annotations missing in tests

--- integrationtests/all-embedded-query-it/src/test/java/org/infinispan/all/embeddedquery/testdomain/hsearch/TransactionHS.java
+++ integrationtests/all-embedded-query-it/src/test/java/org/infinispan/all/embeddedquery/testdomain/hsearch/TransactionHS.java
@@ -42,6 +42,7 @@ public class TransactionHS implements Transaction, Serializable {
 
    @Field(store = Store.YES, analyze = Analyze.NO)
    @NumericField
+   @SortableField
    private double amount;
 
    @Field(store = Store.YES, analyze = Analyze.NO)
