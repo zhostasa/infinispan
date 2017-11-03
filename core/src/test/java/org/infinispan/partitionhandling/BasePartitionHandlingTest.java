7bd74396e6fd4aa7316fa4b18c83108a4373064e
Ryan Emerson <remerson@redhat.com>
2017-11-03 11:56:18 +0000

    JDG-1441 ISPN-8440 Merge tests refactored to reduce repetition and allow for more split-brain scenarios

--- core/src/test/java/org/infinispan/partitionhandling/BasePartitionHandlingTest.java
+++ core/src/test/java/org/infinispan/partitionhandling/BasePartitionHandlingTest.java
@@ -43,7 +43,7 @@ import org.testng.annotations.Test;
 
 @Test(groups = "functional", testName = "partitionhandling.BasePartitionHandlingTest")
 public class BasePartitionHandlingTest extends MultipleCacheManagersTest {
-   private static Log log = LogFactory.getLog(BasePartitionHandlingTest.class);
+   protected static Log log = LogFactory.getLog(BasePartitionHandlingTest.class);
 
    private final AtomicInteger viewId = new AtomicInteger(5);
    protected int numMembersInCluster = 4;
