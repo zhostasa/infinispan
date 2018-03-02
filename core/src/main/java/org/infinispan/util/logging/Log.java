60cc164d6da34a41894b593962ec146d3d6d28d0
Tristan Tarrant <ttarrant@redhat.com>
2018-03-02 16:22:19 +0100

    JDG-1504 ISPN-8907 Prevent CacheManager start when global state is corrupt
    - perform validation of the global state file on startup

--- core/src/main/java/org/infinispan/util/logging/Log.java
+++ core/src/main/java/org/infinispan/util/logging/Log.java
@@ -1631,4 +1631,7 @@ public interface Log extends BasicLogger {
 
    @Message(value = "The configuration is immutable", id = 515)
    UnsupportedOperationException immutableConfiguration();
+
+   @Message(value = "The state file for '%s' is invalid. Startup halted to prevent further corruption of persistent state", id = 516)
+   CacheConfigurationException invalidPersistentState(String globalScope);
 }
